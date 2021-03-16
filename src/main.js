891a8299f86a4b86b76cf5983fa3ee92144504a2
Tyler Sinks <tyler.sinks@gmail.com>
2021-03-15 23:13:35 -0400

    add all UI, biz logic, html
    Co-authored-by: Saoud Rana <saoud@saoud.dev>
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,26 @@
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './css/styles.css';
+import WeatherService from './weather-service.js';
+
+function clearFields() {
+  $('#location').val('');
+  $('.showErrors').text('');
+  $('.showHumidity').text('');
+  $('.showTemp').text('');
+}
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvin is ${body.main.temp} degrees`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
